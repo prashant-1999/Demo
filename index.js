683fc50b771a559e9a53a545f2a5cdb4ee444f7d
Prashant Gupta <63060363+prashant-1999@users.noreply.github.com>
2022-07-22 17:08:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function checkRegister(){
+    var checkBox = document.getElementById("gridCheck");
+
+    var text = document.getElementById("text");
+
+    
+    if (checkBox.checked == true){
+        alert("Hi, Welcome to the page");
+    } else {
+        alert("please agree to terms & conditions to register")
+    }
+}
